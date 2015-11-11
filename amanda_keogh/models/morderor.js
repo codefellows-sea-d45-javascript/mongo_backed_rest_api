cb726cff1e32bfc239e0b3e6af90e0bdba82d9f5
Amanda Keogh <amanda.keogh@gmail.com>
2015-11-11 12:33:38 -0800

    create morderor model

---
+++ amanda_keogh/models/morderor.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+var morderorSchema = new mongoose.Schema({
+  name: string,
+  morders: {name: String, verb: String},
+  morderedBy: {name: String, verb: String}
+})
+
+modules.exports = exports = mongoose.model('Morderor', morderorSchema);
