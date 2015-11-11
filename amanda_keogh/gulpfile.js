512a4e813582469394eac3015245bf617faacc22
Amanda Keogh <amanda.keogh@gmail.com>
2015-11-11 12:47:25 -0800

    write gulpfile

---
+++ amanda_keogh/gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
+
+var appFiles = ['server.js', __dirname + '/routes/**/*.js', __dirname + '/models/**/*.js', __dirname + '/lib/**/*.js'];
+var testFiles = ['gulpfile.js', __dirname + "/test/**/*.js"];
+
+gulp.task('jshint:appfiles', function() {
+  return gulp.src(appFiles)
+    .pipe(jshint({
+      node: true,
+    }))
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('jshint:testfiles', function() {
+  return gulp.src(testFiles)
+    .pipe(jshint({
+      node: true,
+      globals: {
+        before: true,
+        it: true,
+        expect: true
+      }
+    }))
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(appFiles, ['jshint:appfiles']);
+  gulp.watch(testFiles, ['jshint:appfiles']);
+})
+
+gulp.task('default', ['watch']);
--- amanda_keogh/models/morderor.js
+++ amanda_keogh/models/morderor.js
@@ -4,6 +4,6 @@ var morderorSchema = new mongoose.Schema({
   name: string,
   morders: {name: String, verb: String},
   morderedBy: {name: String, verb: String}
-})
+});
 
 modules.exports = exports = mongoose.model('Morderor', morderorSchema);
