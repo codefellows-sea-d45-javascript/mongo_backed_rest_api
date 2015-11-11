d492ffe1708b5fabce91438d7ae537b6ce252ba1
Mike Skalandunas <mskalandunas@gmail.com>
2015-11-11 11:26:40 -0800

    add test file

---
+++ mike_skalandunas/README.md
@@ -0,0 +1,9 @@
+###Docs
+
+To use:
+
+```npm install -g superagent-cli```
+
+Run the mongo server and then the node server:
+
+```
---
+++ mike_skalandunas/test/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var fs = require('fs');
+var mongoose = require('mongoose');
+var chaiHttp = require('chai-http');
+var chai = require('chai');
+var expect = chai.expect;
+
+chai.use(chaiHttp);
+
