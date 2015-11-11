161dcd5fd7e819304a00407882b425b41c5536a7
Mike Skalandunas <mskalandunas@gmail.com>
2015-11-10 17:58:47 -0800

    add tapir router

---
+++ mike_skalandunas/routes/tapirs_routes.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var Tapir = require(__dirname + '/../models/tapir');
+var handleError = require(__dirname + '/../lib/handleServerError');
+
+var tapirsRouter = module.exports = exports = express.Router();
+
+tapirsRouter.get('/tapirs', function(req, res) {
+  Tapir.find({}, function(err, data) {
+    if (err) return handleError(err, res);
+
+    res.json(data);
+  });
+});
+
+tapirsRouter.post('/tapirs', bodyParser.json(), function(req, res) {
+  var newTapir = new Tapir(req.body);
+  newTapir.save(function(err, data) {
+    if (err) return handleError(err, res);
+
+    res.json(data);
+  });
+});
+
+tapirsRouter.put('/tapirs/:id', bodyParser.json(), function(req, res) {
+  var tapirData = req.body;
+  delete tapirData._id;
+  Tapir.update({_id: req.params.id}, tapirData, function(err) {
+    if (err) return handleError(err, res);
+
+    res.json({msg: 'success!'});
+  });
+});
+
+tapirsRouter.delete('/tapirs/:id', function(req, res) {
+  Tapir.remove({_id: req.params.id}, function(err) {
+    if (err) return handleError(err, res);
+
+    res.json({msg: 'success!'});
+  });
+});
