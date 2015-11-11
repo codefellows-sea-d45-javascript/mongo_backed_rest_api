2d2243c1ce84ae9bf27a3f4665d5bb2527b93780
Mike Skalandunas <mskalandunas@gmail.com>
2015-11-11 11:01:07 -0800

    add non-CRUD req

--- mike_skalandunas/routes/tapirs_routes.js
+++ mike_skalandunas/routes/tapirs_routes.js
@@ -4,6 +4,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var Tapir = require(__dirname + '/../models/tapir');
 var handleError = require(__dirname + '/../lib/handleServerError');
+var currentTime = require(__dirname + '/../lib/date.js');
 
 var tapirsRouter = module.exports = exports = express.Router();
 
@@ -20,6 +21,7 @@ tapirsRouter.post('/tapirs', bodyParser.json(), function(req, res) {
   newTapir.save(function(err, data) {
     if (err) return handleError(err, res);
 
+    currentTime();
     res.json(data);
   });
 });
