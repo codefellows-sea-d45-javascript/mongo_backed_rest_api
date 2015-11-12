8a276ef7f8bc8208b064565b3a3403ac4a447b97
Iryna Bondarenko <irusiabondarenko@gmail.com>
2015-11-11 18:28:42 -0800

    Fix style

--- iryna_bondarenko/models/country.js
+++ iryna_bondarenko/models/country.js
@@ -1,17 +1,17 @@
 var mongoose = require('mongoose');
 
 var countrySchema = new mongoose.Schema({
-  name: {type: String, required: true},
-  places: String,
-  durationDays: {type: Number, validate: {
-	  validator: function(value) {
-		  Number.max = 365;
-		  return value == 1;
-	  },
-	  message: "Please, enter correct duration"
+	name: {type: String, required: true},
+	places: String,
+	durationDays: {type: Number, validate: {
+		validator: function(value) {
+			Number.max = 365;
+			return value == 1;
 		},
-  },
-  description: String
+		message: "Please, enter correct duration"
+		}
+	},
+	description: String
 });
 
 module.exports = mongoose.model('Country', countrySchema);
