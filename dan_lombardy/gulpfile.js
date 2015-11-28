09a28a37ddf64b617f3e849671a1510a9d582b1c
DanLombardy <lombardy.dan@gmail.com>
2015-11-28 15:13:31 -0800

    Add front end angular files. Modify server, .gitignore, gulpfile, and package.json

--- dan_lombardy/gulpfile.js
+++ dan_lombardy/gulpfile.js
@@ -3,8 +3,24 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
-var appFiles = ['server.js', 'lib/**/*.js', 'bin/**/*.js'];
+var jshintAppFiles = ['server.js', 'lib/**/*.js', 'bin/**/*.js'];
 var testFiles = ['./test/**/*.js'];
+var webpack = require('webpack-stream');
+
+gulp.task('static:dev', function(){
+  gulp.src('app/**/*.html')
+  .pipe(gulp.dest('build/'));
+});
+
+gulp.task('webpack:dev', function(){
+  gulp.src('app/js/entry.js')
+  .pipe(webpack({
+    output:{
+      filename: 'bundle.js'
+    }
+  }))
+  .pipe(gulp.dest('build/'));
+});
 
 gulp.task('mocha:test', function(){
   return gulp.src(testFiles)
@@ -25,12 +41,12 @@ gulp.task('jshint:test', function(){
   .pipe(jshint.reporter('default'));
 });
 
-gulp.task('jshint: app', function(){
-  return gulp.src(appFiles)
+gulp.task('jshint:app', function(){
+  return gulp.src(jshintAppFiles)
   .pipe(jshint({}))
   .pipe(jshint.reporter('default'));
 });
 
 gulp.task('jshint', ['jshint:test', 'jshint:app']);
 gulp.task('mocha', ['mocha:test']);
-gulp.task('default', ['jshint', 'mocha:test']);
+gulp.task('build', ['webpack:dev', 'static:dev']);
