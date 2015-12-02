7558241352480d74a02acb6b1b9f5c74afb1d3bd
Amanda Keogh <amanda.keogh@gmail.com>
2015-12-02 12:19:46 -0800

    add two-way data binding

--- amanda_keogh/app/js/entry.js
+++ amanda_keogh/app/js/entry.js
@@ -4,4 +4,5 @@ var angular = window.angular;
 var morderMens = angular.module('morderMens', []);
 morderMens.controller('IntroController', ['$scope', function($scope) {
   $scope.intro = 'You thought Rock/Paper/Scissors was intense. You were wrong!';
+  $scope.choice = 'Rock'; //default
 }])
--- amanda_keogh/server.js
+++ amanda_keogh/server.js
@@ -6,20 +6,7 @@ var morderorsRouter = require(__dirname + '/routes/morderors_routes.js');
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/morderor_dev');
 
-app.get("/:filename", function(req, res, next) {
-  fs.stat(__dirname + "/build/" + req.params.filename, function(err, stats){
-    if (err) {
-      console.log(err);
-      return next();
-    }
-
-    if (!stats.isFile) return next();
-
-    var file = fs.createReadStream(__dirname + '/build/' + req.params.filename);
-    file.pipe(res);
-
-  })
-})
+app.use(express.static(__dirname + '/build'));
 
 app.use('/api', morderorsRouter);
 
