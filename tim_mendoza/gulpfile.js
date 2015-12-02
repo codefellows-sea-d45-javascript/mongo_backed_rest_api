51a5ebd30f383d849368da55a50da3a6068f3153
Tim Mendoza <timmydoza@gmail.com>
2015-12-02 01:03:57 -0800

    add minify and uglify to gulpfile

--- tim_mendoza/gulpfile.js
+++ tim_mendoza/gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var webpack = require('webpack-stream');
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify');
+
 
 gulp.task('static:dev', function() {
   gulp.src('app/**/*.html')
@@ -16,5 +19,19 @@ gulp.task('webpack:dev', function() {
   .pipe(gulp.dest('build/'));
 });
 
+gulp.task('minify:dev', function() {
+  gulp.src('build/bundle.js')
+  .pipe(minify())
+  .pipe(gulp.dest('build/'));
+});
+
+gulp.task('uglify:dev', function() {
+  gulp.src('build/bundle.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('build/'));
+});
+
 gulp.task('build:dev', ['static:dev', 'webpack:dev']);
 gulp.task('default', ['build:dev']);
+
+gulp.watch(['**/*', '!build/*'], ['default']);
