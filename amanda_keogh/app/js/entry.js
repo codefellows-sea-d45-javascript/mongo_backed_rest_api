04dc26998f5a793ad5720d455fb968bdc6458461
Amanda Keogh <amanda.keogh@gmail.com>
2015-12-02 11:48:05 -0800

    add client side, build process

---
+++ amanda_keogh/app/js/entry.js
@@ -0,0 +1,7 @@
+require('angular/angular');
+var angular = window.angular;
+
+var morderMens = angular.module('morderMens', []);
+morderMens.controller('IntroController', ['$scope', function($scope) {
+  $scope.intro = 'You thought Rock/Paper/Scissors was intense. You were wrong!';
+}])
--- amanda_keogh/server.js
+++ amanda_keogh/server.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var mongoose = require('mongoose');
 var express = require('express');
 var app = express();
@@ -5,7 +6,26 @@ var morderorsRouter = require(__dirname + '/routes/morderors_routes.js');
 
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/morderor_dev');
 
+app.get("/:filename", function(req, res, next) {
+  fs.stat(__dirname + "/build/" + req.params.filename, function(err, stats){
+    if (err) {
+      console.log(err);
+      return next();
+    }
+
+    if (!stats.isFile) return next();
+
+    var file = fs.createReadStream(__dirname + '/build/' + req.params.filename);
+    file.pipe(res);
+
+  })
+})
+
 app.use('/api', morderorsRouter);
+
+app.use(function(req, res) {
+  res.status(404).send('file too stupid to be found');
+})
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('server listening');
