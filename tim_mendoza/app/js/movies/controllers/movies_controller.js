24e386cc0834727455395294b54cc66935f6bfe4
Tim Mendoza <timmydoza@gmail.com>
2015-12-02 11:59:31 -0800

    update index.html and movies_controllers.js to include read and delete options

--- tim_mendoza/app/js/movies/controllers/movies_controller.js
+++ tim_mendoza/app/js/movies/controllers/movies_controller.js
@@ -1,5 +1,39 @@
 module.exports = function(app) {
-  app.controller('$scope', ['$scope', '$http', function($scope, $http) {
+  app.controller('MoviesController', ['$scope', '$http', function($scope, $http) {
+    $scope.movies = [];
 
+    $scope.getAll = function() {
+      $http.get('/api/movies').then(
+        function(res) {
+          $scope.movies = res.data;
+        },
+        function(res) {
+          console.log(res);
+        }
+      );
+    };
+
+    $scope.create = function(newMovie) {
+      if (newMovie.actors) newMovie.actors = newMovie.actors.split(', ');
+      $http.post('/api/movies', newMovie).then(
+        function(res) {
+          $scope.movies.push(res.data);
+        },
+        function(res) {
+          console.log(res);
+        }
+      );
+    };
+
+    $scope.remove = function(movie) {
+      $http.delete('/api/movies' + movie._id).then(
+        function(res) {
+          $scope.getAll();
+        },
+        function(res) {
+          console.log(res);
+        }
+      );
+    };
   }]);
 };
