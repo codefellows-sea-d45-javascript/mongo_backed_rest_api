3a36481b0b4c1850e34b317c958b917f2dc3e7bb
Tim Mendoza <timmydoza@gmail.com>
2015-12-03 14:52:12 -0800

    add test for getAll()

--- tim_mendoza/test/client/movies_controller_test.js
+++ tim_mendoza/test/client/movies_controller_test.js
@@ -9,7 +9,7 @@ describe('the movies controller', function() {
   beforeEach(angular.mock.module('MoviesApp'));
 
   beforeEach(angular.mock.inject(function($rootScope, $controller) {
-    $scope = $rootScope;
+    $scope = $rootScope.$new();
     $ControllerConstructor = $controller;
   }));
 
@@ -18,5 +18,25 @@ describe('the movies controller', function() {
     expect(typeof $scope).toBe('object');
     expect(typeof controller).toBe('object');
     expect(Array.isArray($scope.movies)).toBe(true);
+  });
+
+  describe('REST request function', function() {
+    beforeEach(angular.mock.inject(function(_$httpBackend_, $rootScope) {
+      $httpBackend = _$httpBackend_;
+      $scope = $rootScope.$new();
+      $ControllerConstructor('MoviesController', {$scope: $scope});
+    }));
+
+    afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should add an array to movies with a GET all', function() {
+      $httpBackend.expectGET('/api/movies').respond(200, [{_id: 1, title:'thing'}]);
+      $scope.getAll();
+      $httpBackend.flush();
+      expect($scope.movies[0].title).toBe('thing');
+    });
   });
 });
