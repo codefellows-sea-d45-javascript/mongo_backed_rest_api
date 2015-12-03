1ba6573661ab27bc58989a25709f47d682823a51
DanLombardy <lombardy.dan@gmail.com>
2015-12-02 22:13:58 -0800

    Modify routes for only 3 tries. Add test for auto delete/self destruct of msg.

--- dan_lombardy/routes/message_routes.js
+++ dan_lombardy/routes/message_routes.js
@@ -28,17 +28,17 @@ messagesRouter.get('/messages/*', function(req, res){
     var alert = "a self destruct count for the client";
 
   Message.find({oneWordTitle: title}, function(err, data){
-    errorHandler(err, res);
+    if (err) return errorHandler(err, res);
 
     var adder = 0;
 
     if(data.length === 0){
-      console.log("I am firing");
       console.log("Client tried to get an object that doesnt exist");
       return res.status(404).json({errmsg: "That message does not exist"});
     }
 
-    if(data[0].views >= 3){
+
+    if(data[0].views >= 2){
       console.log("Message with title " + data[0].oneWordTitle + " self destructed!");
       data[0].destructMessage = "BOOM! Your message self-destructed!";
       data[0].views = data[0].views +1;
@@ -47,29 +47,23 @@ messagesRouter.get('/messages/*', function(req, res){
         errorHandler(err, res);
 
         console.log("Message was deleted from database.");
-
-        res.json(data);
+        data.destructMessage = "BOOM! Your message self-destructed!";
+        res.json({"destructMessage": "BOOM! Your message self-destructed!"} );
       });
     }
 
-    if(data[0].views === 2){
-      alert = "If you GET this message again it will self destruct!";
-      console.log("Message has one view left");
-
-      updateDBSendClient({oneWordTitle: title},{$set:{destructMessage: alert, views:adder}}, res);
-    }
-
     if(data[0].views === 1){
-      alert = "You can GET this message 2 more time and it self destructs!";
+      alert = "You can GET this message 1 more time and it self destructs!";
       console.log("Message has two views left");
+      adder = 2;
 
       updateDBSendClient({oneWordTitle: title},{$set:{destructMessage: alert, views:adder}}, res);
     }
 
     if(data[0].views === 0){
-      alert = "You can GET this message 3 more times and it self destructs!";
-      data[0].destructMessage = alert;
+      alert = "You can GET this message 2 more times and it self destructs!";
       console.log("Message has three views left");
+      adder = 1;
 
       updateDBSendClient({oneWordTitle: title},{$set:{destructMessage: alert, views:adder}}, res);
     }
