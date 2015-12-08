c604e26c3751b4d8732bfb376ac15f43a3ab7efb
DanLombardy <lombardy.dan@gmail.com>
2015-12-08 11:37:43 -0800

    Add initial css styles. Add gulp watch and css concat/minify

--- dan_lombardy/gulpfile.js
+++ dan_lombardy/gulpfile.js
@@ -6,6 +6,9 @@ var mocha = require('gulp-mocha');
 var jshintAppFiles = ['server.js', 'lib/**/*.js', 'bin/**/*.js'];
 var testFiles = ['./test/**/*.js'];
 var webpack = require('webpack-stream');
+var minifyCss = require('gulp-minify-css');
+var concatCss = require('gulp-concat-css');
+var gulpWatch = require('gulp-watch');
 
 gulp.task('static:dev', function(){
   gulp.src('app/**/*.html')
@@ -55,8 +58,25 @@ gulp.task('webpack:test', function(){
     }
   }))
   .pipe(gulp.dest('test/client/'));
-})
+});
+
+gulp.task('css:dev', function(){
+  return gulp.src([
+    'app/css/normalize.css',
+    'app/css/base.css',
+    'app/css/layout.css',
+    'app/css/module.css',
+    'app/css/state.css',
+    'app/css/theme.css'])
+  .pipe(concatCss('styles.min.css'))
+  .pipe(minifyCss())
+  .pipe(gulp.dest('build/'));
+});
+
+gulp.task('css.watch', function(){
+  gulp.watch('./app/css/**/*.css', ['css:dev']);
+});
 
 gulp.task('jshint', ['jshint:test', 'jshint:app']);
 gulp.task('mocha', ['mocha:test']);
-gulp.task('build', ['webpack:dev', 'static:dev']);
+gulp.task('build', ['webpack:dev', 'static:dev', 'css:dev']);
