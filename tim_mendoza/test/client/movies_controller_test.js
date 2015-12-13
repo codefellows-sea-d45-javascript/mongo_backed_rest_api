68013695eb8b645075b834816c8f0d2d7cb993ff
Tim Mendoza <timmydoza@gmail.com>
2015-12-13 13:01:11 -0800

    add PUT test

--- tim_mendoza/test/client/movies_controller_test.js
+++ tim_mendoza/test/client/movies_controller_test.js
@@ -42,7 +42,7 @@ describe('the movies controller', function() {
     it('should be able to create a new movie', function() {
       $httpBackend.expectPOST('/api/movies', {title: 'testmovie'}).respond(200, {title: 'a different movie'});
       expect($scope.movies.length).toBe(0);
-      expect($scope.newMovie).toBe(undefined); //this will need to be changed with defaults, if any. Set to null? Yes
+      expect($scope.newMovie).toBe(undefined);
       $scope.newMovie = {title: 'testmovie'};
       $scope.create($scope.newMovie);
       $httpBackend.flush();
@@ -51,7 +51,7 @@ describe('the movies controller', function() {
     });
 
     it('should be able to delete a movie', function() {
-      $httpBackend.expectDELETE('/api/movies/12345').respond(200, 'ok');
+      $httpBackend.expectDELETE('/api/movies/12345').respond(200);
       $scope.movies[0] = {
         _id: 12345,
         title: 'test'
@@ -60,6 +60,22 @@ describe('the movies controller', function() {
       $scope.remove($scope.movies[0]);
       $httpBackend.flush();
       expect($scope.movies[0].title).toBe('othermovie');
+    });
+    it('should be able to update a movie', function() {
+      $httpBackend.expectPUT('/api/movies/23456').respond(200, 'update succesful');
+      $scope.movies = [
+        {title: 'movieone'},
+        {title: 'movietwo', year: 1999, _id: '23456'},
+        {title: 'moviethree'}
+      ];
+      $scope.edit($scope.movies[1]);
+      expect($scope.movies[1].title).toBe('moviethree');
+      expect($scope.editing).toBe(true);
+      expect($scope.newMovie.title).toBe('movietwo');
+      $scope.newMovie.title = 'movieedit';
+      $scope.submitEdit($scope.newMovie);
+      $httpBackend.flush();
+      expect($scope.movies[2].title).toBe('movieedit');
     });
   });
 });
