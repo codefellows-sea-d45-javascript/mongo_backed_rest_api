30c04a55c75a119c899e6582dc46d835227f1261
DanLombardy <lombardy.dan@gmail.com>
2015-12-14 12:32:59 -0800

    Add header_directive

--- dan_lombardy/app/js/entry.js
+++ dan_lombardy/app/js/entry.js
@@ -3,4 +3,5 @@ var angular = window.angular;
 
 var selfDestructApp = angular.module('SelfDestructApp', []);
 require('./services/services')(selfDestructApp);
+require('./directives/directives')(selfDestructApp);
 require('./messages/messages')(selfDestructApp);
